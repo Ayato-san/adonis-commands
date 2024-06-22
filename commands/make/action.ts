ca62b5061c72488ca43ce6f9bc6356969395ac89
Ayato-san <ayatosan.pro@gmail.com>
2024-06-22 23:38:54 +0200

    feat: add configuration usage

--- commands/make/action.ts
+++ commands/make/action.ts
@@ -6,18 +6,18 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 import { BaseCommand, args, flags } from '@adonisjs/core/ace'
 import type { CommandOptions } from '@adonisjs/core/types/ace'
 import StringBuilder from '@poppinss/utils/string_builder'
 
+import type { ProjectConfig } from '../../src/define_config.js'
 import { stubsRoot } from '../../stubs/main.js'
 
 export default class MakeAction extends BaseCommand {
   static commandName = 'make:action'
   static description = 'Create a new action class'
 
-  static options: CommandOptions = {}
+  static options: CommandOptions = { startApp: true }
 
   @args.string({ description: 'The name of the action' })
   declare name: string
@@ -55,14 +55,15 @@ export default class MakeAction extends BaseCommand {
   /**
    * Makes path to the actions directory
    */
-  actionsPath(entityName: string): string {
-    return this.app.makePath('app/actions', this.actionFileName(entityName))
+  actionsPath(entityName: string, appPath: string = 'app'): string {
+    return this.app.makePath(appPath, '/actions', this.actionFileName(entityName))
   }
 
   async run() {
+    const cmd: ProjectConfig = await this.app.container.make('command.config')
     const entity = this.app.generators.createEntity(this.name)
     const name = this.actionName(entity.name)
-    const filePath = this.actionsPath(entity.name)
+    const filePath = this.actionsPath(entity.name, cmd.folders?.app)
 
     const codemods = await this.createCodemods()
     await codemods.makeUsingStub(stubsRoot, 'make/action/main.stub', {
--- commands/make/presenter.ts
+++ commands/make/presenter.ts
@@ -6,18 +6,18 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 import { BaseCommand, args, flags } from '@adonisjs/core/ace'
 import type { CommandOptions } from '@adonisjs/core/types/ace'
 import StringBuilder from '@poppinss/utils/string_builder'
 
+import type { ProjectConfig } from '../../src/define_config.js'
 import { stubsRoot } from '../../stubs/main.js'
 
 export default class MakePresenter extends BaseCommand {
   static commandName = 'make:presenter'
   static description = 'Create a new presenter class'
 
-  static options: CommandOptions = {}
+  static options: CommandOptions = { startApp: true }
 
   @args.string({ description: 'The name of the presenter' })
   declare name: string
@@ -55,14 +55,15 @@ export default class MakePresenter extends BaseCommand {
   /**
    * Makes path to the presenters directory
    */
-  presentersPath(entityName: string): string {
-    return this.app.makePath('app/presenters', this.presenterFileName(entityName))
+  presentersPath(entityName: string, appPath: string = 'app'): string {
+    return this.app.makePath(appPath, '/presenters', this.presenterFileName(entityName))
   }
 
   async run() {
+    const cmd: ProjectConfig = await this.app.container.make('command.config')
     const entity = this.app.generators.createEntity(this.name)
     const name = this.presenterName(entity.name)
-    const filePath = this.presentersPath(entity.name)
+    const filePath = this.presentersPath(entity.name, cmd.folders?.app)
 
     const codemods = await this.createCodemods()
     await codemods.makeUsingStub(stubsRoot, 'make/presenter/main.stub', {
