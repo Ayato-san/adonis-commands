8f6f8ad856f1d153e2d40246111abf8a2e2ef672
Ayato-san <ayatosan.pro@gmail.com>
2024-06-22 22:54:36 +0200

    chore: setup some config

--- configure.ts
+++ configure.ts
@@ -7,10 +7,6 @@
  * file that was distributed with this source code.
  */
 
-import type Configure from '@adonisjs/core/commands/configure'
+import type ConfigureCommand from '@adonisjs/core/commands/configure'
 
-import { stubsRoot } from './stubs/main.js'
-
-export async function configure(command: Configure) {
-  const codemods = await command.createCodemods()
-}
+export async function configure(_command: ConfigureCommand) {}
